e4dc1919455fdd71114d57cbe973c928cda75869
John Carlson <JCJohnCarlson@gmail.com>
2024-11-23 20:29:09 -0500

    Added bootstrap css imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
